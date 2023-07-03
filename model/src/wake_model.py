da8b6afd7b7c8431aa9da4a41fab1b5ca6a67fec
Leppy-oss <70586381+Leppy-oss@users.noreply.github.com>
2023-07-03 12:42:06 -0500

    cannot get the tick labels to work

--- model/src/wake_model.py
+++ model/src/wake_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import sqrt, radians, cos, sin, log
 from matplotlib import pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 # coeff. of thrust
 C_t = 0.8
@@ -14,13 +15,13 @@ epsilon = sqrt(beta)
 
 
 # diameter of the turbine in m
-d_0 = 0.4
+d_0 = 0.2
 
-width = 15 # of simulation
-height = 4 # of simulation
+width = 5 * d_0 # of simulation
+height = d_0 # of simulation
 step = 0.01 # of simulation
 
-yaw = radians(-30)
+yaw = radians(0.01)
 
 theta_c0 = 0.3 * yaw / cos(yaw) * (1 - sqrt(1 - C_t * cos(yaw)))
 sigma_u0 = sqrt(C_t * (sin(yaw) + 1.978 * cos(yaw) * theta_c0) / (72 * theta_c0))
@@ -39,11 +40,31 @@ x, y = np.meshgrid(np.arange(0, width, step), np.arange(-height / 2, height / 2,
 
 z = wake(x, y)
 
-fig, ax = plt.subplots(1, 1)
-
+fig, ax = plt.subplots(1)
 plt.imshow(z, cmap='rainbow')
-ax.set_title('Mathematical Wake Model')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
+
+ax.set_xticks([0, 20, 40, 60, 80, 100])
+ax.set_xticklabels(['1', '2', '3', '4', '5', '6'])
+print(ax.get_xticklabels())
+# plt.xticks(x_positions, [3, 3.5, 4, 4.5, 5])
+
+yt = np.arange(0, height, step) # the grid to which your data corresponds
+ny = yt.shape[0]
+no_labels = 3 # how many labels to see on axis x
+step_y = int(ny / (no_labels - 1)) # step between consecutive labels
+y_positions = np.arange(0,ny,step_y) # pixel count at label position
+y_positions
+plt.yticks(y_positions, [-0.5, 0.5])
+plt.title('Mathematical Wake Model')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+# ax.set_title('Mathematical Wake Model')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+# ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 plt.show()
 # plt.waitforbuttonpress()
